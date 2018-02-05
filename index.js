82f151e4722db936aedb0fa7da09a56fb9663af6
Santosh SN <nasturtus@gmail.com>
2018-02-05 07:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeli, customerName){
   katzDeli.push(customerName);
-  return `Welcome, ${name}. You are number ${katzDeli.length - 1} in line.`
+  return `Welcome, ${customerName}. You are number ${katzDeli.length - 1} in line.`
 }
