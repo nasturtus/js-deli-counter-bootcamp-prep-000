2b6d051a7964d84ff882af0b6df51c86d6ee99b6
Santosh SN <nasturtus@gmail.com>
2018-02-05 07:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function nowServing(katzDeli) {
     return `Currently serving ${katzDeli.shift()}.`
   }
 }
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var str = "The line is currently: "
+    for (var i = 0; i < katzDeli.length; i++) {
+      str += 
+    }
+  }
+}
