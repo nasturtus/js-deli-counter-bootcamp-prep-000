e0dbdb6a93a62c46f60056c73f59452cc154cfaa
Santosh SN <nasturtus@gmail.com>
2018-02-05 07:49:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(katzDeli, customerName){
   katzDeli.push(customerName);
   return `Welcome, ${customerName}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return ""
+  }
+}
