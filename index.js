324674ebe15f8305d39ae3cf0af32f46789c7a0f
Santosh SN <nasturtus@gmail.com>
2018-02-05 08:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function currentLine(katzDeli) {
     for (var i = 0; i < katzDeli.length; i++) {
       str+= (i+1) + `. ${katzDeli[i]}, `
     }
-    return str;
+    return str.slice(0, str.length - 1);
   }
 }
